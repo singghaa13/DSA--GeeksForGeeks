d0cc73f527f6b5089a6edc716af827f350a5b9de
Ashutoshsingh12220262 <91286609+singghaa13@users.noreply.github.com>
2024-07-18 16:43:51 +0530

    Update Rightmost_different_bit.cpp

--- Bit Magic/Rightmost_different_bit.cpp
+++ Bit Magic/Rightmost_different_bit.cpp
@@ -1,8 +1,19 @@
-int posOfRightMostDiffBit(int m, int n)
-{
-    
-    int s =m^n;
-    return ffs(s);
-    
-    
-}
+    int posOfRightMostDiffBit(int m, int n) {
+        // XOR of m and n will have set bits where m and n differ.
+        int s = m ^ n;
+        
+        // If s is 0, it means there are no different bits.
+        if (s == 0) return -1;
+        
+        // Initialize position counter.
+        int position = 1;
+        
+        // Find the position of the rightmost set bit in s.
+        while ((s & 1) == 0) {
+            s >>= 1;
+            position++;
+        }
+        
+        return position;
+    }
+};
